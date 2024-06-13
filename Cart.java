4725a7685121368987bfe114269799b51d8e27e6
Bennedict Mkhonto <4226279@myuwc.ac.za>
2024-06-13 15:36:26 +0200

    Updated Cart

--- Cart.java
+++ Cart.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+
 import java.util.*;
 
 public class Cart {
     public String order;
     public double price;
-    ArrayList<Cart>Arr=new ArrayList<Cart>();
+    
 
     public Cart(){
 
@@ -29,32 +30,28 @@ public class Cart {
     public double getPrice(){
         return price;
     }
-
-    public void clearCart(){
-        order= null;
-        price=0.0;
-    }
     public void addCart(){
         Scanner input =new Scanner(System.in);
         price=0.0;
         order= null;
         System.out.println("Here is the list of available meals: \n"+
-                          "1. 1X PAP, 1X SHORT RIBS AND 1X COCA-COLA 2L, PRICE: R150.00 \n"+
-                          "2. 1X PAP, 1X FULL CHICKEN AND 1X COCA-COLA 2L, PRICE: 170.00 \n"+
-                          "3. 1X PAP, 1X POARK AND 1x COCA-COLA 2L, PRICE: R120 \n"+
+                          "1. 1X PAP, 1X SHORT RIBS AND 1X COCA-COLA 2L, PRICE: R125.00 \n"+
+                          "2. 1X PAP, 1X FULL CHICKEN AND 1X COCA-COLA 2L, PRICE: 150.00 \n"+
+                          "3. 1X PAP, 1X POARK AND 1x COCA-COLA 2L, PRICE: R105 \n"+
                           "4. 1X PAP, PRICE: R7.50 \n"+
                           "5. 1X COCA-COLA: 750ml, PRICE: R16.50 \n"+
                           "6. 1X COCA-COLA 2L, PRICE: R25.70 \n"+
                           "7. 1X SHORT RIBS, PRICE: R100.00 \n"+
                           "8. 1X FULL CHICKEN, PRICE: R120.00 \n"+
-                          "9. 1X POARK, PRICE: R95.00 \n"+
-                          "10. 1X LARGE CHIPS, PRICE: R60.00 \n"+
-                          "11. 1X MEDIUM CHIPS, PRICE: R45.00 \n"+
-                          "12. 1X SMALL CHIPS, PRICE: R30.00 \n"+
-                          "13. 1X SPRING WATER 750ML, PRICE: R12.00 \n"+
-                          "14. 1X SPRING WATER : R17.50 \n"+
-                          "15. 1X SAUSAGE, PRICE: R12.00 \n"+
-                          "16. To exit");
+                          "9. 1X HALF CHICKEN, PRICE: R60.00 \n"+
+                          "10. 1X POARK, PRICE: R95.00 \n"+
+                          "11. 1X LARGE CHIPS, PRICE: R30.00 \n"+
+                          "12. 1X MEDIUM CHIPS, PRICE: R25.00 \n"+
+                          "13. 1X SMALL CHIPS, PRICE: R20.00 \n"+
+                          "14. 1X SPRING WATER 750ML, PRICE: R12.00 \n"+
+                          "15. 1X SPRING WATER 20L : R17.50 \n"+
+                          "17. 1X SAUSAGE, PRICE: R10.00 \n"+
+                          "18. To exit \n");
         System.out.println("Please selent the number associated with what you are ordering: ");
         int ordernum=input.nextInt();
         
@@ -62,41 +59,37 @@ public class Cart {
             if(ordernum==1){
                 System.out.println("How many do you like to order: ");
                 int quantity = input.nextInt();
-                order="1X PAP, 1X SHORT RIBS AND 1X COCA-COLA 2L";
-                price+=150.00*quantity;
+                order=quantity+"X PAP,"+quantity+"X SHORT RIBS AND "+quantity+"X COCA-COLA 2L";
+                price+=125.00*quantity;
                 
                 Cart cart1 =new Cart(order, price);
-                Arr.add(cart1);
                 break;
             }
 
             if(ordernum==2){
                 System.out.println("How many do you like to order: ");
                 int quantity2 = input.nextInt();
-                order="1X PAP, 1X FULL CHICKEN AND 1X COCA-COLA 2L";
+                order=quantity2+"X PAP, "+quantity2+"X FULL CHICKEN AND "+quantity2+"X COCA-COLA 2L";
                 price+=150.00*quantity2;
                 Cart cart2 =new Cart(order, price);
-                Arr.add(cart2);
                 break;
             }
 
             if(ordernum==3){
                 System.out.println("How many do you like to order: ");
                 int quantity3 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity3;
+                order=quantity3+"X PAP, "+quantity3+"X POARK AND "+quantity3+"X COCA-COLA 2L";
+                price+=105.00*quantity3;
                 Cart cart3 =new Cart(order, price);
-                Arr.add(cart3);
                 break;
             }
 
             if(ordernum==4){
                 System.out.println("How many do you like to order: ");
                 int quantity4 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity4;
+                order=quantity4+"X PAP";
+                price+=7.50*quantity4;
                 Cart cart4 =new Cart(order, price);
-                Arr.add(cart4);
                 break;
             }
 
@@ -104,10 +97,9 @@ public class Cart {
             if(ordernum==5){
                 System.out.println("How many do you like to order: ");
                 int quantity5 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity5;
+                order=quantity5+"X COCA-COLA: 750ml";
+                price+=16.50*quantity5;
                 Cart cart5 =new Cart(order, price);
-                Arr.add(cart5);
                 break;
             }
 
@@ -115,10 +107,9 @@ public class Cart {
             if(ordernum==6){
                 System.out.println("How many do you like to order: ");
                 int quantity6 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity6;
+                order=quantity6+"X COCA-COLA 2L";
+                price+=25.50*quantity6;
                 Cart cart6 =new Cart(order, price);
-                Arr.add(cart6);
                 break;
             }
 
@@ -126,10 +117,9 @@ public class Cart {
             if(ordernum==7){
                 System.out.println("How many do you like to order: ");
                 int quantity7 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity7;
+                order=quantity7+"X SHORT RIBS";
+                price+=100.00*quantity7;
                 Cart cart7 =new Cart(order, price);
-                Arr.add(cart7);
                 break;
             }
 
@@ -137,110 +127,133 @@ public class Cart {
             if(ordernum==8){
                 System.out.println("How many do you like to order: ");
                 int quantity8 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity8;
+                order=quantity8+"X FULL CHICKEN";
+                price+=120.00*quantity8;
                 Cart cart8 =new Cart(order, price);
-                Arr.add(cart8);
                 break;
             }
-
-
             if(ordernum==9){
-                System.out.println("How many do you like to order: ");
-                int quantity9 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity9;
-                Cart cart9 =new Cart(order, price);
-                Arr.add(cart9);
+                System.out.println("How many do you want to order: ");
+                int quantity9=input.nextInt();
+                order=quantity9+"X HALF-CHICKEN";
+                price=60.00*quantity9;
+                Cart cart9=new Cart(order,price);
                 break;
+
             }
 
 
             if(ordernum==10){
                 System.out.println("How many do you like to order: ");
                 int quantity10 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity10;
+                order=quantity10+"X POARK, PRICE";
+                price+=95.00*quantity10;
                 Cart cart10 =new Cart(order, price);
-                Arr.add(cart10);
                 break;
             }
 
 
             if(ordernum==11){
                 System.out.println("How many do you like to order: ");
-                int quantity11 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity11;
-                Cart cart11 =new Cart(order, price);
-                Arr.add(cart11);
+                int quantity10 = input.nextInt();
+                order=quantity10+"X LARGE CHIPS, PRICE";
+                price+=30.00*quantity10;
+                Cart cart10 =new Cart(order, price);
                 break;
             }
 
 
             if(ordernum==12){
                 System.out.println("How many do you like to order: ");
-                int quantity12 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity12;
-                Cart cart12 =new Cart(order, price);
-                Arr.add(cart12);
+                int quantity11 = input.nextInt();
+                order=quantity11+"X MEDIUM CHIPS, PRICE ";
+                price+=25.00*quantity11;
+                Cart cart11 =new Cart(order, price);
+
                 break;
             }
 
 
             if(ordernum==13){
                 System.out.println("How many do you like to order: ");
-                int quantity13 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity13;
-                Cart cart13 =new Cart(order, price);
-                Arr.add(cart13);
+                int quantity12 = input.nextInt();
+                order=quantity12+"X SMALL CHIPS, PRICE";
+                price+=20.00*quantity12;
+                Cart cart12 =new Cart(order, price);
                 break;
             }
 
 
             if(ordernum==14){
                 System.out.println("How many do you like to order: ");
+                int quantity13 = input.nextInt();
+                order=quantity13+"X SPRING WATER 750ML";
+                price+=12.00*quantity13;
+                Cart cart13 =new Cart(order, price);
+                break;
+            }
+
+
+            if(ordernum==15){
+                System.out.println("How many do you like to order: ");
                 int quantity14 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity14;
+                order=quantity14+"X SPRING WATER 20L : R17.50";
+                price+=17.50*quantity14;
                 Cart cart14 =new Cart(order, price);
-                Arr.add(cart14);
                 break;
             }
             if(ordernum==15){
                 System.out.println("How many do you like to order: ");
                 int quantity15 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity15;
+                order=quantity15+"X SAUSAGE";
+                price+=10.00*quantity15;
                 Cart cart15 =new Cart(order, price);
-                Arr.add(cart15);
                 break;
             }
-
-            if(ordernum==16){
-                System.out.println("How many do you like to order: ");
-                int quantity16 = input.nextInt();
-                order="1X PAP, 1X POARK AND 1x COCA-COLA 2L";
-                price+=150.00*quantity16;
-                Cart cart16 =new Cart(order, price);
-                Arr.add(cart16);
+            else{
+                System.out.println("The number you've entered is not one of the options");
                 break;
             }
         }
     }
-    public void showCart(){
-        for(int i=0;i<Arr.size();i++){
-            System.out.println(Arr.get(i).order+" "+Arr.get(i).getPrice()+"\n");
-        }
-    }
+    
 
 
     public static void main(String[] args){
-        Cart cart =new Cart();
-        cart.addCart();
-        cart.showCart();
+        ArrayList<Cart>Arr=new ArrayList<Cart>();
+        Scanner input=new Scanner(System.in);
+        System.out.println("Here is the list of available meals: \n"+
+        "1. 1X PAP, 1X SHORT RIBS AND 1X COCA-COLA 2L, PRICE: R125.00 \n"+
+        "2. 1X PAP, 1X FULL CHICKEN AND 1X COCA-COLA 2L, PRICE: 150.00 \n"+
+        "3. 1X PAP, 1X POARK AND 1x COCA-COLA 2L, PRICE: R105 \n"+
+        "4. 1X PAP, PRICE: R7.50 \n"+
+        "5. 1X COCA-COLA: 750ml, PRICE: R16.50 \n"+
+        "6. 1X COCA-COLA 2L, PRICE: R25.70 \n"+
+        "7. 1X SHORT RIBS, PRICE: R100.00 \n"+
+        "8. 1X FULL CHICKEN, PRICE: R120.00 \n"+
+        "9. 1X HALF CHICKEN, PRICE: R60.00 \n"+
+        "10. 1X POARK, PRICE: R95.00 \n"+
+        "11. 1X LARGE CHIPS, PRICE: R30.00 \n"+
+        "12. 1X MEDIUM CHIPS, PRICE: R25.00 \n"+
+        "13. 1X SMALL CHIPS, PRICE: R20.00 \n"+
+        "14. 1X SPRING WATER 750ML, PRICE: R12.00 \n"+
+        "15. 1X SPRING WATER 20L : R17.50 \n"+
+        "17. 1X SAUSAGE, PRICE: R10.00 \n"+
+        "18. To exit \n");
+        System.out.println("How many orders will you would you like to make up to a maximum of 10 orders: ");
+        int numoforders=input.nextInt();
+        for(int i=0;i<numoforders;i++){
+            Cart cart=new Cart();
+            cart.addCart();
+            Arr.add(cart);
+            
+        }
+        for(int i=0;i<Arr.size();i++){
+            System.out.println(Arr.get(i).order+" "+Arr.get(i).getPrice()+"\n");
+        }
+        
+
+       
 
     }
     
